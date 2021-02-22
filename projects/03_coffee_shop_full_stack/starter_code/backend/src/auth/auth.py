32b064f70d94dcdb721ffeb5bf26e66e7c7d0714
ozieblo-repository <ozieblo.michal@icloud.com>
2021-02-22 18:14:51 +0100

    backup; running the server part

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-11opmcqr.eu.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'http://127.0.0.1:5000'
 
 ## AuthError Exception
 '''
